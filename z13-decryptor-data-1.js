cdd21706ed646249e2a643d6e11ec858fffb7e57
David Oranchak <doranchak@gmail.com>
2026-06-14 11:08:31 -0400

    added

---
+++ z13-decryptor-data-1.js
@@ -0,0 +1,15 @@
+/* names */
+export default [
+    { text: "Luis Rory Reily", score: 12 },
+    { text: "L Jacob ODonald", score: 11 },
+    { text: "Eddie Resendes", score: 10 },
+    { text: "Steve Pete West", score: 9 },
+    { text: "Gary Lyle Large", score: 8 },
+    { text: "Reagan Adalard", score: 6 },
+    { text: "Ernie Zerenner", score: 5 },
+    { text: "Derick C Acorda", score: 4 },
+    { text: "Elliot OSoules", score: 3 },
+    { text: "Eddy A Marander", score: 2 },
+    { text: "Barney E Yearby", score: 1 },
+    { text: "Shannon E Nease", score: 0 },
+];
---
+++ z13-decryptor-data-2.js
@@ -0,0 +1,23 @@
+/* phrases */
+export default [
+    { text: "On TV A War Actor", score: 0 },
+    { text: "Sarah The Horse", score: 0 },
+    { text: "Dr Eat A Totpedo", score: 0 },
+    { text: "MAYBE MERELY MR", score: 0 },
+    { text: "I SAW ALAN AGAIN", score: 0 },
+    { text: "CHIEF OF AFRICA", score: 0 },
+    { text: "A FINE VENETIAN", score: 0 },
+    { text: "AVIS A CANADIAN", score: 0 },
+    { text: "KANE POPS PUNKS", score: 0 },
+    { text: "OBSCENE NELSON", score: 0 },
+    { text: "SLAVE PETE LAST", score: 0 },
+    { text: "CHASTITY TRACY", score: 0 },
+    { text: "KRISPY PAPRIKA", score: 0 },
+    { text: "EATEN AND NOTED", score: 0 },
+    { text: "THAT RARER NATE", score: 0 },
+    { text: "EVIL EYE DEFIED", score: 0 },
+    { text: "AS REAL AS AURAS", score: 0 },
+    { text: "SO IM A SATANIST", score: 0 },
+    { text: "I WAS A MAN AGAIN", score: 0 },
+    { text: "A DOG A CAT A GOAT", score: 0 }
+];
